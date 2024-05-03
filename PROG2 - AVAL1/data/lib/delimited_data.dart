218de9c1e393dc3e102adc4807545c281c7fbad5
Pedro Barros <49743096+Pedrut@users.noreply.github.com>
2024-05-03 08:49:23 -0300

    Delete PROG2 - AVAL1/data directory

--- PROG2 - AVAL1/data/lib/delimited_data.dart
+++
@@ -1,7 +0,0 @@
-// Classe abstrata para fontes de dados delimitados por algum caractere
-part of exchange;
-
-abstract class DelimitedData extends Data {
-  String
-      get delimiter; // Obtém o caractere delimitador utilizado na fonte de dados
-}
